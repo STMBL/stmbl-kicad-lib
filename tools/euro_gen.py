ce791969166534192b6e646692a84e5fc4ceb5fa
crinq <crinq@crinq.de>
2022-11-11 16:59:52 +0100

    add kefa KF2EDGS 3.5mm

--- tools/euro_gen.py
+++ tools/euro_gen.py
@@ -2,7 +2,7 @@
 import sys
 import time
 
-pins = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+pins = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def line(x0, y0, x1, y1, layer = "F.SilkS"):
     return("  (fp_line (start " + str(x0) + " " + str(y0) + ") (end "  + str(x1) + " " + str(y1) + ") (layer " + layer + ") (width 0.15))\n")
@@ -260,13 +260,156 @@ def eurostyle_RM381_gen(pins):
 
         f.write(footer())
 
+def KEFA_KF2EDGSR_RM350_gen(pins):
+    rm = 3.5
+    filename = "KEFA_KF2EDGSR_RM" + str(rm) + "_2x" + str(pins)
+    pad_size = 2.5
+    drill_size = 1.5
+    drill_pad_size = drill_size + 0.3
 
-for p in pins:    
-    eurostyle_RM762_up_gen(p)
-    eurostyle_RM762_gen(p)
-    eurostyle_RM508_up_gen(p)
-    eurostyle_RM508_gen(p)
-    eurostyle_RM350_up_gen(p)
-    eurostyle_RM350_gen(p)
-    eurostyle_RM381_up_gen(p)
-    eurostyle_RM381_gen(p)
+    with open(filename + ".kicad_mod", mode = "w+") as f:
+        f.write(header("stmbl", filename))
+
+        # outline
+        f.write(rect(-2.15, 2.5 + 0.5, rm * (pins - 1) + 2.15, 2.5 + 0.5 - 10.5))
+        f.write(circle(0, 0, 1.5, 0))
+        f.write(rect(-1.5, -1.5, 1.5, 1.5, "B.SilkS"))
+
+        # plug outline
+        f.write(rect(-2.15, 2.5 + 0.5 - 10.5, rm * (pins - 1) + 2.15, 2.5 + 0.5 - 10.5 - 16))
+        
+        for i in range(pins):
+            f.write(th_pad(i, rm * i, 0, drill_pad_size, drill_size))
+            f.write(bot_pad(i, rm * i, 0, pad_size))
+            f.write(th_pad(i + pins, rm * i, 2.5, drill_pad_size, drill_size))
+            f.write(bot_pad(i + pins, rm * i, 2.5, pad_size))
+
+        f.write(footer())
+
+def KEFA_KF2EDGSRMG_RM350_gen(pins):
+    rm = 3.5
+    filename = "KEFA_KF2EDGSRMG_RM" + str(rm) + "_2x" + str(pins)
+    pad_size = 2.5
+    drill_size = 1.5
+    drill_pad_size = drill_size + 0.3
+
+    with open(filename + ".kicad_mod", mode = "w+") as f:
+        f.write(header("stmbl", filename))
+
+        # outline
+        f.write(rect(-2.15, 2.5 + 0.5, rm * (pins - 1) + 2.15, 2.5 + 0.5 - 10.5))
+        f.write(rect(-2.15 - 3.1, 2.5 + 0.5, rm * (pins - 1) + 2.15 + 3.1, 2.5 + 0.5 - 10.5))
+
+
+        f.write(circle(0, 0, 1.5, 0))
+        f.write(rect(-1.5, -1.5, 1.5, 1.5, "B.SilkS"))
+
+        # plug outline
+        f.write(rect(-2.15, 2.5 + 0.5 - 10.5, rm * (pins - 1) + 2.15, 2.5 + 0.5 - 10.5 - 16))
+        f.write(rect(-2.15 - 3.1, 2.5 + 0.5 - 10.5, -2.15, 2.5 + 0.5 - 10.5 - 16))
+        f.write(rect(rm * (pins - 1) + 2.15, 2.5 + 0.5 - 10.5, rm * (pins - 1) + 2.15 + 3.1, 2.5 + 0.5 - 10.5 - 16))
+
+        for i in range(pins):
+            f.write(th_pad(i, rm * i, 0, drill_pad_size, drill_size))
+            f.write(bot_pad(i, rm * i, 0, pad_size))
+            f.write(th_pad(i + pins, rm * i, 2.5, drill_pad_size, drill_size))
+            f.write(bot_pad(i + pins, rm * i, 2.5, pad_size))
+
+        f.write(footer())
+
+def KEFA_KF2EDGSVMG_RM350_gen(pins):
+    rm = 3.5
+    filename = "KEFA_KF2EDGSVMG_RM" + str(rm) + "_2x" + str(pins)
+    pad_size = 2.5
+    drill_size = 1.5
+    drill_pad_size = drill_size + 0.3
+
+    with open(filename + ".kicad_mod", mode = "w+") as f:
+        f.write(header("stmbl", filename))
+
+        # outline
+        f.write(rect(-5.25, -2.55, rm * (pins - 1) + 5.25, -2.55 + 10.7))
+        f.write(line(-(3.5 + 0.8) / 2, -2.55, -(3.5 + 0.8) / 2, 5.5 + 2.55))
+        f.write(line((3.5 + 0.8) / 2 + rm * (pins - 1), -2.55, +(3.5 + 0.8) / 2 + rm * (pins - 1), 5.5 + 2.55))
+        f.write(line(-5.25, 4.5, -(3.5 + 0.8) / 2, 4.5))
+        f.write(line(5.25 + rm * (pins - 1), 4.5, (3.5 + 0.8) / 2 + rm * (pins - 1), 4.5))
+        f.write(circle(-3.5, 0, -2.75, 0))
+        f.write(circle(3.5 + rm * (pins - 1), 0, 2.75 + rm * (pins - 1), 0))
+        f.write(circle(0, 0, 1.5, 0))
+        f.write(rect(-1.5, -1.5, 1.5, 1.5, "B.SilkS"))
+
+        # plug outline
+        f.write(rect(-(3.5 + 0.8) / 2 - 3.1, -(13.25 - 5.5) / 2, rm * (pins - 1) + (3.5 + 0.8) / 2 + 3.1, (13.25 - 5.5) / 2 + 5.5))
+        
+        for i in range(pins):
+            f.write(th_pad(i, rm * i, 0, drill_pad_size, drill_size))
+            f.write(bot_pad(i, rm * i, 0, pad_size))
+            f.write(th_pad(i + pins, rm * i, 5.5, drill_pad_size, drill_size))
+            f.write(bot_pad(i + pins, rm * i, 5.5, pad_size))
+            f.write(line(-1.5 + rm * i, -1.5, 1.5 + rm * i, -1.5))
+            f.write(line(-1.5 + rm * i, 1.5 + 5.5, 1.5 + rm * i, 1.5 + 5.5))
+
+            f.write(line(-1.5 + rm * i, -1.5, -1.5 + rm * i, 2))
+            f.write(line( 1.5 + rm * i, -1.5,  1.5 + rm * i, 2))
+            f.write(line(-1.5 + rm * i, 3, -1.5 + rm * i, 1.5 + 5.5))
+            f.write(line( 1.5 + rm * i, 3,  1.5 + rm * i, 1.5 + 5.5))
+
+            f.write(line(-1.5 + rm * i, 2, -0.75 + rm * i, 3))
+            f.write(line( 1.5 + rm * i, 2,  0.75 + rm * i, 3))
+            f.write(line(-0.75 + rm * i, 3, -1.5 + rm * i, 3))
+            f.write(line( 0.75 + rm * i, 3,  1.5 + rm * i, 3))
+
+        f.write(footer())
+
+def KEFA_KF2EDGSV_RM350_gen(pins):
+    rm = 3.5
+    filename = "KEFA_KF2EDGSV_RM" + str(rm) + "_2x" + str(pins)
+    pad_size = 2.5
+    drill_size = 1.5
+    drill_pad_size = drill_size + 0.3
+
+    with open(filename + ".kicad_mod", mode = "w+") as f:
+        f.write(header("stmbl", filename))
+
+        # outline
+        f.write(rect(-(3.5 + 0.8) / 2, -2.55, rm * (pins - 1) + (3.5 + 0.8) / 2, -2.55 + 10.7))
+        f.write(circle(0, 0, 1.5, 0))
+        f.write(rect(-1.5, -1.5, 1.5, 1.5, "B.SilkS"))
+
+        # plug outline
+        f.write(rect(-(3.5 + 0.8) / 2, -(13.25 - 5.5) / 2, rm * (pins - 1) + (3.5 + 0.8) / 2, (13.25 - 5.5) / 2 + 5.5))
+        
+        for i in range(pins):
+            f.write(th_pad(i, rm * i, 0, drill_pad_size, drill_size))
+            f.write(bot_pad(i, rm * i, 0, pad_size))
+            f.write(th_pad(i + pins, rm * i, 5.5, drill_pad_size, drill_size))
+            f.write(bot_pad(i + pins, rm * i, 5.5, pad_size))
+            f.write(line(-1.5 + rm * i, -1.5, 1.5 + rm * i, -1.5))
+            f.write(line(-1.5 + rm * i, 1.5 + 5.5, 1.5 + rm * i, 1.5 + 5.5))
+
+            f.write(line(-1.5 + rm * i, -1.5, -1.5 + rm * i, 2))
+            f.write(line( 1.5 + rm * i, -1.5,  1.5 + rm * i, 2))
+            f.write(line(-1.5 + rm * i, 3, -1.5 + rm * i, 1.5 + 5.5))
+            f.write(line( 1.5 + rm * i, 3,  1.5 + rm * i, 1.5 + 5.5))
+
+            f.write(line(-1.5 + rm * i, 2, -0.75 + rm * i, 3))
+            f.write(line( 1.5 + rm * i, 2,  0.75 + rm * i, 3))
+            f.write(line(-0.75 + rm * i, 3, -1.5 + rm * i, 3))
+            f.write(line( 0.75 + rm * i, 3,  1.5 + rm * i, 3))
+
+        f.write(footer())
+
+
+for p in pins:
+    KEFA_KF2EDGSR_RM350_gen(p)
+    KEFA_KF2EDGSRMG_RM350_gen(p)
+    KEFA_KF2EDGSV_RM350_gen(p)  
+    KEFA_KF2EDGSVMG_RM350_gen(p)  
+    # eurostyle_RM762_up_gen(p)
+    # eurostyle_RM762_gen(p)
+    # eurostyle_RM508_up_gen(p)
+    # eurostyle_RM508_gen(p)
+    # eurostyle_RM350_up_gen(p)
+    # eurostyle_RM350_gen(p)
+    # eurostyle_RM381_up_gen(p)
+    # eurostyle_RM381_gen(p)
